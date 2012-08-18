63b79a975bcf057096404c3ea72d62560a2a102b
Greg Price <price@mit.edu>
2012-08-18 13:20:27 -0400

    Index $LOADED_FEATURES so require isn't so slow
    Still up: freeze the strings.

--- vm.c
+++ vm.c
@@ -1486,6 +1486,8 @@ rb_vm_mark(void *ptr)
 	RUBY_MARK_UNLESS_NULL(vm->load_path_snapshot);
 	RUBY_MARK_UNLESS_NULL(vm->expanded_load_path);
 	RUBY_MARK_UNLESS_NULL(vm->loaded_features);
+	RUBY_MARK_UNLESS_NULL(vm->loaded_features_snapshot);
+	RUBY_MARK_UNLESS_NULL(vm->loaded_features_index);
 	RUBY_MARK_UNLESS_NULL(vm->top_self);
 	RUBY_MARK_UNLESS_NULL(vm->coverages);
 	rb_gc_mark_locations(vm->special_exceptions, vm->special_exceptions + ruby_special_error_count);
